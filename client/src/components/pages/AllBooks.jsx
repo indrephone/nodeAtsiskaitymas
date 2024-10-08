c11e2470ba465bcc47228f80e00e8834b2cb033a
indrephone <indrephone@gmail.com>
2024-10-08 15:16:07 +0300

    add Formik form for filters

--- client/src/components/pages/AllBooks.jsx
+++ client/src/components/pages/AllBooks.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import BookCard from "../UI/molecules/BookCard";
+import { useFormik } from "formik";
 
 const StyledSection = styled.section`
   > h1 {
@@ -66,6 +67,21 @@ const StyledSection = styled.section`
 const AllBooks = () => {
   const [books, setBooks] = useState([]);
 
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      genres_in: [],
+      year_gte: '',
+      year_lte: '',
+      isAvailable: false, // For "Available"
+      isNotAvailable: false // For "Not Available"
+    },
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+  
+
   useEffect(() => {
     fetch(`http://localhost:5500/books`)
       .then(res => res.json())
@@ -79,11 +95,135 @@ const AllBooks = () => {
         {/* Filter everything */}
         <div>
           <h4>Filter</h4>
-          <form>
-            <div>
-              {/* Formik form content goes here */}
-            </div>
-          </form>
+          <form onSubmit={formik.handleSubmit}> 
+  <div>
+    <label htmlFor="title">Title:</label>
+    <input 
+      type="text" 
+      id="title" 
+      name="title"
+      value={formik.values.title}
+      onChange={formik.handleChange}
+    />
+  </div>
+
+  {/* Genres Fieldset */}
+  <fieldset>
+    <legend>Select Genres:</legend>
+    <div>
+      <label htmlFor="classic">Classic</label>
+      <input 
+        type="checkbox" 
+        name="genres_in" 
+        id="classic"
+        value="Classic"
+        onChange={formik.handleChange}
+        checked={formik.values.genres_in.includes('Classic')}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="fiction">Fiction</label>
+      <input 
+        type="checkbox" 
+        name="genres_in" 
+        id="fiction"
+        value="Fiction"
+        onChange={formik.handleChange}
+        checked={formik.values.genres_in.includes('Fiction')}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="non_fiction">Non-Fiction</label>
+      <input 
+        type="checkbox" 
+        name="genres_in" 
+        id="non_fiction"
+        value="NonFiction"
+        onChange={formik.handleChange}
+        checked={formik.values.genres_in.includes('NonFiction')}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="mystery">Mystery</label>
+      <input 
+        type="checkbox" 
+        name="genres_in" 
+        id="mystery"
+        value="Mystery"
+        onChange={formik.handleChange}
+        checked={formik.values.genres_in.includes('Mystery')}
+      />
+    </div>
+
+    <div>
+      <label htmlFor="romance">Romance</label>
+      <input 
+        type="checkbox" 
+        name="genres_in" 
+        id="romance"
+        value="Romance"
+        onChange={formik.handleChange}
+        checked={formik.values.genres_in.includes('Romance')}
+      />
+    </div>
+  </fieldset>
+
+  <div>
+    <label htmlFor="year_gte">Year from:</label>
+    <input 
+      type="number" 
+      id="year_gte" 
+      name="year_gte"
+      value={formik.values.year_gte}
+      onChange={formik.handleChange}
+    />
+  </div>
+
+  <div>
+    <label htmlFor="year_lte">Year to:</label>
+    <input 
+      type="number" 
+      id="year_lte" 
+      name="year_lte"
+      value={formik.values.year_lte}
+      onChange={formik.handleChange}
+    />
+  </div>
+
+  {/* Availability Fieldset */}
+  <fieldset>
+    <legend>Availability</legend>
+    <div>
+      <label>
+        <input 
+          type="checkbox" 
+          name="isAvailable" 
+          onChange={formik.handleChange}
+          checked={formik.values.isAvailable}
+        />
+        Available
+      </label>
+    </div>
+
+    <div>
+      <label>
+        <input 
+          type="checkbox" 
+          name="isNotAvailable" 
+          onChange={formik.handleChange}
+          checked={formik.values.isNotAvailable}
+        />
+        Not Available
+      </label>
+    </div>
+  </fieldset>
+
+  <button type="submit">Submit</button>
+</form>
+
         </div>
 
         {/* Sort everything */}
